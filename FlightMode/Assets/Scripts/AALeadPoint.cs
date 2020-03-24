2aff4c58138f75f447b165bb34944a9e0cf3c279
Emil Virolainen <virolainen.emil@gmail.com>
2020-03-24 16:07:04 +0200

    Turrets lead their shots

---
+++ FlightMode/Assets/Scripts/AALeadPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AALeadPoint : MonoBehaviour {
+
+	ShipMovement ship;
+
+	void Start() {
+		ship = GameObject.Find("Ship").GetComponent<ShipMovement>();
+	}
+
+	void Update() {
+		transform.localPosition = new Vector3(0, 0, ship.moveSpeed);
+	}
+}
--- FlightMode/Assets/Scripts/TurretAI.cs
+++ FlightMode/Assets/Scripts/TurretAI.cs
@@ -7,38 +7,40 @@ public class TurretAI : MonoBehaviour {
 	public GameObject bullet;
 	public GameObject barrel;
 	public GameObject ammoSpawn;
+	public GameObject target;
 	public GameObject player;
 	public float accuracy;
 	public float shootForce;
 	public float range;
 	public bool targetInSight;
+	public float smallestTargetSpeed;
 
 	float counter;
 	public float fireRate;
 
 	void Start() {
-
+		target = GameObject.Find("AALeadPoint");
+		player = GameObject.Find("Ship");
 	}
 
 	void Update() {
-		transform.LookAt(player.transform, Vector3.forward);
+
+		transform.LookAt(target.transform, Vector3.forward);
 		float y = transform.eulerAngles.y;
 		transform.eulerAngles = new Vector3(0, y, 0);
 
-		barrel.transform.LookAt(player.transform, Vector3.forward);
+		barrel.transform.LookAt(target.transform, Vector3.forward);
 		float x = barrel.transform.eulerAngles.x;
 		barrel.transform.eulerAngles = new Vector3(x, y, 0);
 
-		RaycastHit hit;
-		Vector3 fwd = barrel.transform.TransformDirection(Vector3.forward);
-		Debug.DrawRay(transform.position, fwd * range, Color.red, 0.01f);
-		if (Physics.Raycast(transform.position, fwd, out hit, range) && hit.transform.CompareTag("Player")) {
+		if (Physics.Linecast(transform.position, player.transform.position)) {
 			targetInSight = true;
 		} else {
 			targetInSight = false;
 		}
-
-		if (counter >= fireRate && Vector3.Distance(transform.position, player.transform.position) < range && targetInSight) {
+		if (player.GetComponent<ShipMovement>().moveSpeed > smallestTargetSpeed)
+			shootForce = player.GetComponent<ShipMovement>().moveSpeed;
+		if (counter >= fireRate && Vector3.Distance(transform.position, target.transform.position) < range && targetInSight) {
 			Shoot();
 			counter = 0;
 		} else {
